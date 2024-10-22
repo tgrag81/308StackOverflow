3f54ce3df71262bd06b2f142f4f9cbf07f1e1945
Terence A Gragston <terence.gragston@gmail.com>
2024-10-22 12:51:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ function initialFunction() {
     try {
         initialFunction ();
     } catch (error ) {
+        let counter = 0;
+        if (counter < 7) {
         console.error ("An error occurred:", error);
+        console.log("Counter:", counter);
     }
 function incrementCounter() {
     count ++;
-}}
+}}}
 
 function getCounterValue () {
     return counter;
